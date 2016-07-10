3bdcc741bcbbd1399c34bee2f0f195a7a970e06e
Mark Alston <malston@pivotal.io>
2016-07-10 10:44:09 -0600

    Run integration tests on travis ci

---
+++ integration-test/integration-test-travis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -evx
+
+function health_check() {
+    HEALTH_CHECK_READY=
+    while [ "$HEALTH_CHECK_READY" = "" ]; do
+      echo "Creating container..."
+      HEALTH_CHECK_READY=$(curl -s "$PUBLIC_IP:$1/$2"; echo);
+      sleep 1
+    done
+}
+
+# Export DOCKER public IP
+export PUBLIC_IP="$(echo \"$(echo $DOCKER_HOST)\"| \sed 's/tcp:\/\//http:\/\//g'| \sed 's/:[0-9]\{4,\}//g'| \sed 's/\"//g')"
+
+declare -a ports=("7474" "8761/health" "8888/admin/health")
+# Do health checks on compose services
+for i in "${ports[@]}"
+do
+   health_check "$i"
+done
