1b56b71f9f9427feffcf278f33784b6ef74749ed
Mark Alston <malston@pivotal.io>
2016-07-11 17:25:26 -0600

    Docker inspect rabbit and graphdb services

--- integration-test/build-travis.sh
+++ integration-test/build-travis.sh
@@ -2,12 +2,14 @@
 set -ex
 
 export PUBLIC_IP="locahost"
-export SPRING_NEO4J_HOST=$PUBLIC_IP
-export SPRING_RABBITMQ_HOST=$PUBLIC_IP
+#export SPRING_NEO4J_HOST=$PUBLIC_IP
+#export SPRING_RABBITMQ_HOST=$PUBLIC_IP
+export SPRING_NEO4J_HOST="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' graphdb)"
+export SPRING_RABBITMQ_HOST="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' rabbit)"
 export EUREKA_CLIENT_SERVICEURL_DEFAULTZONE="http://$PUBLIC_IP:8761/eureka/"
 export SPRING_CLOUD_CONFIG_URI="http://$PUBLIC_IP:8888"
 export TWITTER_CRAWLER_HOST="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' crawler)"
 export TWITTER_CRAWLER_PORT="8080"
 # Run integration tests
-mvn clean test -Dtests=integration -Dspring.neo4j.host="graphdb" -Dserver.host=$TWITTER_CRAWLER_HOST -Dremote.server.port=$TWITTER_CRAWLER_PORT || die "'mvn clean test -Dtests=integration' failed" 1
+mvn clean test -Dtests=integration -Dserver.host=$TWITTER_CRAWLER_HOST -Dremote.server.port=$TWITTER_CRAWLER_PORT || die "'mvn clean test -Dtests=integration' failed" 1
 # mvn clean install || die "'mvn clean install' failed" 1
