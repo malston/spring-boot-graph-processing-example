2ef60d0c4317021ba3a422b86fd70eea257659a3
Mark Alston <malston@pivotal.io>
2016-07-10 21:43:29 -0600

    Use localhost for ip

--- integration-test/integration-test-travis.sh
+++ integration-test/integration-test-travis.sh
@@ -5,15 +5,10 @@ function health_check() {
     HEALTH_CHECK_READY=
     while [ "$HEALTH_CHECK_READY" = "" ]; do
       echo "Creating container..."
-      HEALTH_CHECK_READY=$(curl -s "$PUBLIC_IP:$1/$2"; echo);
+      HEALTH_CHECK_READY=$(curl -s "http://localhost:$1/$2"; echo);
       sleep 1
     done
 }
-
-# Export DOCKER public IP
-echo "DOCKER_HOST=$DOCKER_HOST"
-export PUBLIC_IP="$(echo \"$(echo $DOCKER_HOST)\"| \sed 's/tcp:\/\//http:\/\//g'| \sed 's/:[0-9]\{4,\}//g'| \sed 's/\"//g')"
-echo "PUBLIC_IP=$PUBLIC_IP"
 
 declare -a ports=("7474" "8761/health" "8888/admin/health")
 # Do health checks on compose services
