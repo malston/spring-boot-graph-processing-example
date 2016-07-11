412b728809c9d0c26d7b187c6db76edae0c0860e
Mark Alston <malston@pivotal.io>
2016-07-11 15:21:37 -0600

    Add properties to support running remote tests

--- integration-test/build-travis.sh
+++ integration-test/build-travis.sh
@@ -7,7 +7,7 @@ export SPRING_RABBITMQ_HOST=$PUBLIC_IP
 export EUREKA_CLIENT_SERVICEURL_DEFAULTZONE="http://$PUBLIC_IP:8761/eureka/"
 export SPRING_CLOUD_CONFIG_URI="http://$PUBLIC_IP:8888"
 export TWITTER_CRAWLER_HOST="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' crawler)"
-
+export TWITTER_CRAWLER_PORT="8080"
 # Run integration tests
-mvn clean test -Dtests=integration -Dcontainer.ip=$TWITTER_CRAWLER_HOST || die "'mvn clean test -Dtests=integration' failed" 1
+mvn clean test -Dtests=integration -Dserver.host=$TWITTER_CRAWLER_HOST -Dremote.server.port=$TWITTER_CRAWLER_PORT || die "'mvn clean test -Dtests=integration' failed" 1
 # mvn clean install || die "'mvn clean install' failed" 1
--- twitter-rank-crawler/src/test/java/org/kbastani/TwitterCrawlerTests.java
+++ twitter-rank-crawler/src/test/java/org/kbastani/TwitterCrawlerTests.java
@@ -1,42 +1,54 @@
 package org.kbastani;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kbastani.twitter.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.assertEquals;
-
+/**
+ * To support running these tests against an already running service I've added
+ * {@literal host} and {@literal port} properties.
+ * <p>
+ * For example:
+ * <code>mvn clean test -Dtests=integration -Dserver.host=$TWITTER_CRAWLER_HOST -Dremote.server.port=$TWITTER_CRAWLER_PORT</code>
+ * 
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TwitterCrawlerApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port=0")
+@WebIntegrationTest("server.port=0")
 @ActiveProfiles("test")
 public class TwitterCrawlerTests {
 
     @Value("${local.server.port}")
     private int port = 0;
 
-    @Value("${container.ip}")
-    private String host = "0.0.0.0";
+    @Value("${remote.server.port:0}")
+    private int remotePort = 0;
+    
+    @Value("${server.host:localhost}")
+    private String host = "localhost";
 
     @Test
     @IfProfileValue(name="tests", values="integration")
     public void crawlUser() {
-        ResponseEntity<User> user1 = new TestRestTemplate().getForEntity("http://{host}:" + 8080 + "/v1/user/kennybastani", User.class, host);
+    	int hostPort = remotePort != 0 ? remotePort : port;
+    	System.out.println("---------------------------------------");
+    	System.out.println("Testing using endpoint: http://" + host + ":" + hostPort + "/v1/user/kennybastani");
+        ResponseEntity<User> user1 = new TestRestTemplate().getForEntity("http://{host}:" + hostPort + "/v1/user/kennybastani", User.class, host);
         assertEquals(HttpStatus.OK, user1.getStatusCode());
-        ResponseEntity<User> user2 = new TestRestTemplate().getForEntity("http://{host}:" + 8080 + "/v1/user/bridgetkromhout", User.class, host);
+        ResponseEntity<User> user2 = new TestRestTemplate().getForEntity("http://{host}:" + hostPort + "/v1/user/bridgetkromhout", User.class, host);
         assertEquals(HttpStatus.OK, user2.getStatusCode());
-        ResponseEntity<User> user3 = new TestRestTemplate().getForEntity("http://{host}:" + 8080 + "/v1/user/starbuxman", User.class, host);
+        ResponseEntity<User> user3 = new TestRestTemplate().getForEntity("http://{host}:" + hostPort + "/v1/user/starbuxman", User.class, host);
         assertEquals(HttpStatus.OK, user3.getStatusCode());
     }
 }
