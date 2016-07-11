8ba00381958aff80dd15ff40e281e9a030713abd
Mark Alston <malston@pivotal.io>
2016-07-10 21:36:42 -0600

    Trying to set PUBLIC_IP

--- integration-test/integration-test-travis.sh
+++ integration-test/integration-test-travis.sh
@@ -11,7 +11,9 @@ function health_check() {
 }
 
 # Export DOCKER public IP
+echo "DOCKER_HOST=$DOCKER_HOST"
 export PUBLIC_IP="$(echo \"$(echo $DOCKER_HOST)\"| \sed 's/tcp:\/\//http:\/\//g'| \sed 's/:[0-9]\{4,\}//g'| \sed 's/\"//g')"
+echo "PUBLIC_IP=$PUBLIC_IP"
 
 declare -a ports=("7474" "8761/health" "8888/admin/health")
 # Do health checks on compose services
