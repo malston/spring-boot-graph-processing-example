2bee9c2c3770a307cb70324bb57cc532ee87eec0
Mark Alston <malston@pivotal.io>
2016-07-11 18:19:57 -0600

    Debugging travis ci

--- integration-test/build-travis.sh
+++ integration-test/build-travis.sh
@@ -11,5 +11,5 @@ export SPRING_CLOUD_CONFIG_URI="http://$PUBLIC_IP:8888"
 export TWITTER_CRAWLER_HOST="$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' crawler)"
 export TWITTER_CRAWLER_PORT="8080"
 # Run integration tests
-mvn clean test -Dtests=integration -Dserver.host=$TWITTER_CRAWLER_HOST -Dremote.server.port=$TWITTER_CRAWLER_PORT || die "'mvn clean test -Dtests=integration' failed" 1
+mvn clean test -Dtests=integration -Dserver.host=localhost -Dremote.server.port=$TWITTER_CRAWLER_PORT || die "'mvn clean test -Dtests=integration' failed" 1
 # mvn clean install || die "'mvn clean install' failed" 1
